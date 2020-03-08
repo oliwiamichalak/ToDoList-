11b6345b8a589d10cbad4f27a63736a341ccbe3d
oliwia <oliwia.michalak@gmail.com>
2020-03-08 14:28:33 +0000

    Init

---
+++ ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Oliwia Michalak on 08/03/2020.
+//  Copyright © 2020 Oliwia Michalak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
