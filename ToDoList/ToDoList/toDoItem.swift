50e8f9c09ae6b1eef63d86a4e0144d977b868878
oliwia <oliwia.michalak@gmail.com>
2020-03-08 16:25:54 +0000

    ToDo - remove force unwrapping and manage errors

---
+++ ToDoList/ToDoList/toDoItem.swift
@@ -0,0 +1,25 @@
+//
+//  toDoItem.swift
+//  ToDoList
+//
+//  Created by Oliwia Michalak on 08/03/2020.
+//  Copyright © 2020 Oliwia Michalak. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class ToDoItem: NSManagedObject, Identifiable {
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var title: String?
+}
+
+extension ToDoItem {
+    static func getAllToDoItems() -> NSFetchRequest<ToDoItem> {
+        let request: NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest() as! NSFetchRequest<ToDoItem>
+        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
